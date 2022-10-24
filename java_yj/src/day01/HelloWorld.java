9e086e149277b831976d23da4e3ebf7a89429366
yongjin <yongjin3443@gmail.com>
2022-10-24 21:37:19 +0900

    자바 01일차 Hello world 기본 예제

---
+++ java_yj/src/day01/HelloWorld.java
@@ -0,0 +1,18 @@
+package day01;
+
+public class HelloWorld {
+	//프로그램이 시작하려면 main 이 있어야한다. 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello");
+			//콘솔에 원한느 값을 출력하는 기능 
+			//한줄주석
+			/*여러줄 주석 */
+			/** 여러줄 주석, 기능을 설명할 때 활용**/
+			
+			
+			
+	
+	}
+
+}
