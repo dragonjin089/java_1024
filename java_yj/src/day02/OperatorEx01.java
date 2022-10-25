d92e327aa76eb8938c32bdec922dde1d7e9b6bf3
yongjin <yongjin3443@gmail.com>
2022-10-25 21:28:43 +0900

    java 02일차 논리부정연산자.

---
+++ java_yj/src/day02/OperatorEx01.java
@@ -0,0 +1,16 @@
+package day02;
+
+public class OperatorEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//!연산자는 : 참 또는 거짓인 값(변수,리터럴,식)에 앞에옴
+		boolean isMale = true;
+		boolean isFemale =!isMale;
+		
+		System.out.println("남성입니까? : "+isMale);
+		System.out.println("남성입니까? : "+isFemale);
+
+	}
+
+}
