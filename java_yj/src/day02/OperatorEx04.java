ebaf664be223f2adeea84cb5c301c98071aaac35
yongjin <yongjin3443@gmail.com>
2022-10-25 21:31:23 +0900

    java 02일차 비교연산자.

---
+++ java_yj/src/day02/OperatorEx04.java
@@ -0,0 +1,17 @@
+package day02;
+
+public class OperatorEx04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		비교연산자 : 크기비교
+//		결과값이 참또는 거짓이 나옴
+//		조건식 : 연산 결과각 참 또는 거짓이 나오는 식 
+		
+		int age = 18;
+		System.out.println(age + "살은 성인인가? "+(age>=19) );
+		int score = 100;
+		System.out.println(score + "점은 A학점인가? "+(score>=90));
+	}
+
+}
