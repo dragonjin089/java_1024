64d4e9f802b4302608458e34fc7bead95687c223
yongjin <yongjin3443@gmail.com>
2022-10-31 21:48:28 +0900

    java 06일차 정렬

---
+++ java_yj/src/day06/ArraySortEx01.java
@@ -0,0 +1,43 @@
+package day06;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ArraySortEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//버블 정렬을 이용한 배열 정렬
+		//
+		int arr[] = {1,3,5,7,2,8,6,9};
+		
+		for (int i =0 ; i< arr.length -1 ; i++) {
+			for(int j =0 ; j< arr.length -1 - i ;j++) {
+				if(arr[j] > arr[j+1] ) {
+					int tmp = arr[j];
+					arr [j] = arr[j+1];
+					arr[j+1] = tmp;
+				}
+			}
+			for(int tmp : arr) {
+			System.out.print(tmp +"");
+
+		}
+			System.out.println();
+		}
+		System.out.println("======================");
+		int arr2[] = {9,8,7,6,5,4,3,2};
+
+		
+		Arrays.sort(arr2);
+		
+		for(int tmp : arr2) {
+			System.out.print(tmp +"");
+	}
+		Integer arr3[] = {9,8,7,6,5,4,3,2};
+		Arrays.sort(arr3, Collections.reverseOrder());
+		for(int tmp : arr3) {
+			System.out.print(tmp +"");
+		}
+	}
+}
