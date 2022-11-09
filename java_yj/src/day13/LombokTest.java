e6103259b9ddf355c73d2498686e9589a7df0a62
yongjin <yongjin3443@gmail.com>
2022-11-09 21:29:06 +0900

    자바 13일차 lombok test

---
+++ java_yj/src/day13/LombokTest.java
@@ -0,0 +1,27 @@
+package day13;
+
+import lombok.Data;
+
+public class LombokTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestA a = new TestA();
+		a.setNum(10);
+		System.out.println(a.getNum());
+		System.out.println(a);//toString() 확인
+		TestA b = new TestA();
+		b.setNum(10);
+		System.out.println(a.equals(b));// equals()확인
+	}
+
+}
+@Data //@Getter, @Setter, @ToString @EqualsAndHashCode, RequredArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+//@EqualsAndHashCode
+//@RequiredArgsConstructor
+class TestA{
+	private int num;
+}
