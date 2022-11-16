0e7b3683558a61bbd04677a92df11d7537c719f2
yongjin <yongjin3443@gmail.com>
2022-11-16 20:46:55 +0900

    가계부 과제

--- java_yj/src/exampleaccountbook/AccountBook.java
+++ java_yj/src/exampleaccountbook/AccountBook.java
@@ -1,22 +1,37 @@
 package exampleaccountbook;
 
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class AccountBook {
 	ArrayList<Item> list = new ArrayList<Item>();
 	AccountService accountService = new AccountServiceImp();
 	Scanner scan;
+	
 	public void run() {
 		System.out.println("가계부 프로그램 실행");
 		int menu= -1;
 		do {
-			accountService.printMenu();
-			menu = scan.nextInt();
-			accountService.runMenu(list.menu);
+			try {
+				
+				accountService.printMenu();
+				menu = scan.nextInt();
+				accountService.runMenu(list, menu, scan);	
+			}catch(ParseException e) {
+				System.out.println("날짜형태가 잘못되었습니다.");
+			}catch(InputMismatchException e) {
+				System.out.println("잘못된 타입을 입력했습니다.");
+				scan.nextLine();
+			}catch(RuntimeException e) {
+				System.out.println(e.getMessage());
+			}
+			
 		}while(menu!=5);
 		System.out.println("가계부 프로그램 종료");
-	}
+		}
+	
 	public AccountBook(Scanner scan) {
 		if(scan == null) {
 			this.scan = new Scanner(System.in);
--- java_yj/src/exampleaccountbook/Item.java
+++ java_yj/src/exampleaccountbook/Item.java
@@ -1,5 +1,49 @@
 package exampleaccountbook;
 
-public class Item {
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import lombok.Data;
+
+@Data
+public class Item {
+	private String type, purpose, content;
+	private int money;
+	private Date date;
+	
+	public Item(String type, String purpose, String content, int money, String date) throws ParseException {
+		this.type = type;
+		this.purpose = purpose;
+		this.content = content;
+		this.money = money;
+		setDate(date);
+	}
+	public void setDate(String Date) throws ParseException {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		//parse excepiton은 runtime exception이 아니기때문에 예외처리
+		this.date = format.parse(Date);
+	}
+	
+	public String getDateStr() {
+		 SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		 return format.format(date);
+	}
+	public String getMoneyStr() {
+		DecimalFormat df = new DecimalFormat("###,###");
+		return df.format(money);
+	}
+	
+	public void print() {
+		System.out.println("구분 : " + type);
+		System.out.println("분류 : " + purpose);
+		System.out.println("내용 : " + content);
+		System.out.println("금액 : " + getMoneyStr() + "원");
+		System.out.println("일시 : " + getDateStr());
+	}
+	@Override
+	public String toString() {
+		return type + " | " + getDateStr() + " | "+ purpose + " | " + getMoneyStr() + "원"  + " | " + content;
+		}
 }
