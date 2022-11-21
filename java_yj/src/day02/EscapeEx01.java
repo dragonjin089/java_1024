884f926d134de40b47a0ad676363cad6fb617529
yongjin <yongjin3443@gmail.com>
2022-11-21 21:32:00 +0900

    2일차 Escape

--- java_yj/src/day02/EscapeEx01.java
+++ java_yj/src/day02/EscapeEx01.java
@@ -4,11 +4,11 @@ public class EscapeEx01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			char ch1 = '\t';
-			char ch2 = '\n';
-			char ch3 = '\\';
-			char ch4 = '\'';
-			char ch5 = '\"';
+			char ch1 = '\t';// 탭
+			char ch2 = '\n';// 개행
+			char ch3 = '\\';// 역슬래시
+			char ch4 = '\'';// 작은 따옴표 
+			char ch5 = '\"';// 큰따옴표
 			char ch6 = '\u002D'; //002B는 16진수로 10진수 43에 해당
 			
 			System.out.println(ch1+ "안녕하세요");
