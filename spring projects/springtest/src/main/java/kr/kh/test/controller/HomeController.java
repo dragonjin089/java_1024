d0c6f33976271bc116c4f7e544e93d9ea9c1960b
mac-yongjin <homekim089@gamil.com>
2023-02-10 19:45:51 +0900

    Spring DB연결

--- spring projects/springtest/src/main/java/kr/kh/test/controller/HomeController.java
+++ spring projects/springtest/src/main/java/kr/kh/test/controller/HomeController.java
@@ -6,11 +6,14 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import kr.kh.test.service.MemberService;
 
 /**
  * Handles requests for the application home page.
@@ -18,15 +21,15 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	@Autowired
+	MemberService memberService;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ModelAndView home(ModelAndView mv) {
-		
-		
+	public ModelAndView home(ModelAndView mv, Integer num) {
+	
+		String name = memberService.selectMemberName(num);
+		System.out.println(name);
 		mv.setViewName("/main/home");
 		return mv;
 	}
