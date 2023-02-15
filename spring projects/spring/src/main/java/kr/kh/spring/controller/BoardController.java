071b1563354414412eb8f33200d2afb708eaf40a
mac-yongjin <homekim089@gamil.com>
2023-02-15 21:13:55 +0900

    스프링 6일차 게시글 리스트 기본 구성 및 게시글 등록 버튼 추가

---
+++ spring projects/spring/src/main/java/kr/kh/spring/controller/BoardController.java
@@ -0,0 +1,20 @@
+package kr.kh.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+//@RequestMapping(value = "/board")
+//public class BoardController {
+//	
+//	}
+@Controller
+public class BoardController {
+	
+	@RequestMapping(value = "/board/list", method=RequestMethod.GET)
+	public ModelAndView boardList(ModelAndView mv) {
+		mv.setViewName("/board/list");
+		return mv;
+	}
+}
