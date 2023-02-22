ca1f32f5b2ad17a6ecbea9ee63a200b788d8a82a
mac-yongjin <homekim089@gamil.com>
2023-02-22 16:39:08 +0900

    스프링 이메일 인증

--- spring projects/springtest/src/main/java/kr/kh/test/controller/HomeController.java
+++ spring projects/springtest/src/main/java/kr/kh/test/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.kh.test.service.MemberService;
+import kr.kh.test.vo.MemberOKVO;
 import kr.kh.test.vo.MemberVO;
 
 @Controller
@@ -48,18 +49,36 @@ public class HomeController {
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public ModelAndView loginPost(ModelAndView mv,MemberVO member) {
 		MemberVO user = memberService.login(member);
-		mv.addObject("user", user);
-		if(user != null) {
+		//인증한 회원들만 로그인 하도록 
+		if(user != null && user.getMe_authority() > 0) {
+			mv.addObject("user", user);
 			mv.setViewName("redirect:/");
 		}else {
+			if(user!= null) {
+				// 인증 안된 회원 이라고 알려주는 메시지 
+			}
 			mv.setViewName("redirect:/login");
 		}
 		return mv;
 	}
+	
 	@RequestMapping(value = "/logout", method = RequestMethod.POST)
-	public ModelAndView logout(ModelAndView mv,HttpSession session) {
+	public ModelAndView logoutPost(ModelAndView mv, HttpSession session) {
 		if(session != null)
-				session.removeAttribute("user");
+			session.removeAttribute("user");
+		mv.setViewName("redirect:/");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/email/authentication", method = RequestMethod.GET)
+	public ModelAndView emailAuthentication(ModelAndView mv, MemberOKVO mok) {
+		boolean res = memberService.emailAuthentication(mok);
+		if(res) {
+			//인증 성공 메시지
+		}else {
+			//인증 실패 메시지
+		}
+		System.out.println(mok);
 		mv.setViewName("redirect:/");
 		return mv;
 	}
